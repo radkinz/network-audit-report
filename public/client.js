64231ae0ebf3ff1c06dee381f3aac1edd8fe15d7
radkinz <radkins22@students.stab.org>
2021-08-03 18:31:20 -0400

    add drop-down to pc track button

--- public/client.js
+++ public/client.js
@@ -2,8 +2,18 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction2() {
     document.getElementById("myDropdown2").classList.toggle("show");
 }
+
+
+function myFunction3() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
