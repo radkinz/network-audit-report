6baeddc64ec57ce345768d64978b4b4569da3d66
radkinz <radkins22@students.stab.org>
2021-08-03 18:27:30 -0400

    form insert to database & get pc track info

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const http = require("http").Server(app);
 const bodyParser = require("body-parser");
 
+//setup environment variables
+require('dotenv').config();
+const database_path = process.env.DATABASEPATH;
+
 //access the access database
 const ADODB = require('node-adodb');
-const conn = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=networkAudit\\SQL_Schema\\DB - Access.mdb;');
+const conn = ADODB.open(database_path);
 
 //setup app
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +25,7 @@ app.set("views", __dirname + "/views");
 app.get('/', (req, res) => {
     conn.query("SELECT DISTINCT Report.RHost FROM Report;").then(data => {
         console.log(data)
-        res.render("index.html", {Names: data});
+        res.render("index.html", { Names: data });
     }).catch(err => console.log(err));
 })
 
@@ -31,39 +35,55 @@ app.get('/pctrack', (req, res) => {
 })
 
 //return serial nunmber to client for pc track form
-app.post('/serialnumber', (req, res) => {
-    let computer_name = req.body.computername;
-    console.log(computer_name)
-    conn.query("SELECT DISTINCT Item.IField, Item.IValue, Report.RHost FROM Item INNER JOIN Report ON Report.ID = Item.ReportID WHERE Report.RHost='" + computer_name + "'AND ((((Item.ReportID)=Report.ID) AND ((Item.IID)=2572))); ").then(data => {
-        console.log(data)
-    }).catch(err => console.log(err));
-    
+app.post('/pctrackpost', (req, res) => {
+    //insert into pc track database
+    var sql = 'INSERT INTO PcTrack(UserName, ComputerName, SerialNumber, ESNNumber, Building, Department, Room, RemoteComputerName, Function, Instrument, Orientation, Browser, TimeSubmitted) VALUES ("' + req.body.username  + '","' + req.body.computername + '","' + req.body.serialnumber + '","' + req.body.ESNNumber + '","' + req.body.Building + '","' + req.body.Department + '","' + req.body.Room + '","' + req.body.RemoteComputerName + '","' + req.body.Function  + '","' + req.body.Instrument  + '","' + req.body.Orientation  + '","' + req.body.Browser + '","' + req.body.Timestamp + '")'
+
+    conn
+        .execute(sql, 'SELECT @@Identity AS id')
+        .then(data => {
+            console.log(JSON.stringify(data, null, 2));
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+
+
     //go back to homepage
     res.redirect("/")
 })
 
 //display hardware
 app.post
-('/hardware', (req, res) => {
-    console.log(typeof(req.body.submit), req.body)
-    //do not need to worry about sql injection because user cannot type in the input
-    var sql = "SELECT Item.IField, Item.IValue, Report.RHost FROM Item INNER JOIN Report ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ((((Item.ReportID)=Report.ID) AND ((Item.IID)=261)) OR (((Item.IID)=263)) OR (((Item.IID)=517)) OR (((Item.IID)=518)) OR (((Item.IID)=520)) OR (((Item.IID)=530)) OR (((Item.IID)=3864)));" 
-    conn.query(sql).then(data => {
-        console.log(data);
-        res.render("display.html", {Report: data});
-    }).catch(err => console.log(err));
-})
+    ('/hardware', (req, res) => {
+        //do not need to worry about sql injection because user cannot type in the input
+        var sql = "SELECT Item.IField, Item.IValue, Report.RHost FROM Item INNER JOIN Report ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ((((Item.ReportID)=Report.ID) AND ((Item.IID)=261)) OR (((Item.IID)=263)) OR (((Item.IID)=517)) OR (((Item.IID)=518)) OR (((Item.IID)=520)) OR (((Item.IID)=530)) OR (((Item.IID)=3864)));"
+        conn.query(sql).then(data => {
+            console.log(data);
+            res.render("display.html", { Report: data });
+        }).catch(err => console.log(err));
+    })
 
 //display software
 app.post
-('/software', (req, res) => {
-    //do not need to worry about sql injection because user cannot type in the input
-    var sql = "SELECT DISTINCT Report.RHost, Item.IField, Item.IValue FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ((((Item.ReportID)=Report.ID) And ((Item.IID)=538)) Or (((Item.IID)=539)) Or (((Item.IID)=2581)) Or (((Item.IID)=518)) Or (((Item.IID)=520)) Or (((Item.IID)=528)) Or (((Item.IID)=530)) Or (((Item.IID)=1028)) Or (((Item.IID)=514)) Or (((Item.IID)=2583)) Or (((Item.IID)=534)) Or (((Item.IID)=517)) Or (((Item.IID)=3845)) Or (((Item.IID)=3847)) Or (((Item.IID)=540)) Or (((Item.IID)=3859)) Or (((Item.IID)=3864)) Or (((Item.IID)=2306)) Or (((Item.IID)=523)) Or (((Item.IID)=524)) Or (((Item.IID)=526)));";
-    conn.query(sql).then(data => {
-        console.log(data);
-        res.render("display.html", {Report: data});
-    }).catch(err => console.log(err));
-})
+    ('/software', (req, res) => {
+        //do not need to worry about sql injection because user cannot type in the input
+        var sql = "SELECT DISTINCT Report.RHost, Item.IField, Item.IValue FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ((((Item.ReportID)=Report.ID) And ((Item.IID)=538)) Or (((Item.IID)=539)) Or (((Item.IID)=2581)) Or (((Item.IID)=518)) Or (((Item.IID)=520)) Or (((Item.IID)=528)) Or (((Item.IID)=530)) Or (((Item.IID)=1028)) Or (((Item.IID)=514)) Or (((Item.IID)=2583)) Or (((Item.IID)=534)) Or (((Item.IID)=517)) Or (((Item.IID)=3845)) Or (((Item.IID)=3847)) Or (((Item.IID)=540)) Or (((Item.IID)=3859)) Or (((Item.IID)=3864)) Or (((Item.IID)=2306)) Or (((Item.IID)=523)) Or (((Item.IID)=524)) Or (((Item.IID)=526)));";
+        conn.query(sql).then(data => {
+            console.log(data);
+            res.render("display.html", { Report: data });
+        }).catch(err => console.log(err));
+    })
+
+//get pc track info
+app.post
+    ('/pctrackinfo', (req, res) => {
+        conn.query("SELECT DISTINCT PcTrack.ComputerName, PcTrack.SerialNumber, PcTrack.ESNNumber, PcTrack.Building, PcTrack.Department, PcTrack.Room, PcTrack.RemoteComputerName, PcTrack.Function, PcTrack.Instrument, PcTrack.Orientation, PcTrack.Browser FROM PcTrack WHERE PcTrack.ComputerName='" + req.body.submit + "'").then(data => {
+            console.log(data);
+            res.render("pctrackdisplay.html", { Report: data });
+        }).catch(err => console.log(err));
+    })
 
 const listener = http.listen(3000, () => {
     console.log("Your app is listening on port " + 3000);
