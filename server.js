098af204aec33058ac6f8ed2fc3b6aae9811fda0
radkinz <radkins22@students.stab.org>
2021-08-04 18:48:54 -0400

    query database for printer/software count info

--- server.js
+++ server.js
@@ -58,10 +58,10 @@ app.post('/pctrackpost', (req, res) => {
 app.post
     ('/hardware', (req, res) => {
         //do not need to worry about sql injection because user cannot type in the input
-        var sql = "SELECT Item.IField, Item.IValue, Report.RHost FROM Item INNER JOIN Report ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ((((Item.ReportID)=Report.ID) AND ((Item.IID)=261)) OR (((Item.IID)=263)) OR (((Item.IID)=517)) OR (((Item.IID)=518)) OR (((Item.IID)=520)) OR (((Item.IID)=530)) OR (((Item.IID)=3864)));"
+        var sql = "SELECT DISTINCT Item.IField, Item.IValue, Report.RHost FROM Item INNER JOIN Report ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ( (((Item.ReportID)=Report.ID) And ((Item.IID)=538)) Or (((Item.IID)=539)) Or (((Item.IID)=2581)) Or (((Item.IID)=518)) Or (((Item.IID)=520)) Or (((Item.IID)=528)) Or (((Item.IID)=530)) Or (((Item.IID)=1028)) Or (((Item.IID)=514)) Or (((Item.IID)=2583)) Or (((Item.IID)=534)) Or (((Item.IID)=517)) Or (((Item.IID)=3845)) Or (((Item.IID)=3847)) Or (((Item.IID)=540)) Or (((Item.IID)=3859)) Or (((Item.IID)=3864)) Or (((Item.IID)=2306)) Or (((Item.IID)=523)) Or (((Item.IID)=524)) Or (((Item.IID)=526)) Or (((Item.IID)=536)) Or (((Item.IID)=2582)));"
         conn.query(sql).then(data => {
             console.log(data);
-            res.render("display.html", { Report: data });
+            res.render("hardwaredisplay.html", { Report: data });
         }).catch(err => console.log(err));
     })
 
@@ -69,10 +69,10 @@ app.post
 app.post
     ('/software', (req, res) => {
         //do not need to worry about sql injection because user cannot type in the input
-        var sql = "SELECT DISTINCT Report.RHost, Item.IField, Item.IValue FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ((((Item.ReportID)=Report.ID) And ((Item.IID)=538)) Or (((Item.IID)=539)) Or (((Item.IID)=2581)) Or (((Item.IID)=518)) Or (((Item.IID)=520)) Or (((Item.IID)=528)) Or (((Item.IID)=530)) Or (((Item.IID)=1028)) Or (((Item.IID)=514)) Or (((Item.IID)=2583)) Or (((Item.IID)=534)) Or (((Item.IID)=517)) Or (((Item.IID)=3845)) Or (((Item.IID)=3847)) Or (((Item.IID)=540)) Or (((Item.IID)=3859)) Or (((Item.IID)=3864)) Or (((Item.IID)=2306)) Or (((Item.IID)=523)) Or (((Item.IID)=524)) Or (((Item.IID)=526)));";
+        var sql = "SELECT DISTINCT Item.IField, Item.IID, Report.RHost, Item.IDevice FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND  (((Item.IID)=1281));";
         conn.query(sql).then(data => {
             console.log(data);
-            res.render("display.html", { Report: data });
+            res.render("softwaredisplay.html", { Report: data });
         }).catch(err => console.log(err));
     })
 
@@ -84,6 +84,28 @@ app.post
             res.render("pctrackdisplay.html", { Report: data });
         }).catch(err => console.log(err));
     })
+
+app.post('/printerinfo', (req, res) => {
+    conn.query("SELECT DISTINCT Item.IField, Item.IValue, Item.IID, Report.RHost, Item.IDevice FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE (((Item.IID)=1028)) OR (((Item.IID)=536));").then(data => {
+        console.log(data)
+        res.render("printerdisplay.html", { Report: data });
+    }).catch(err => console.log(err));
+})
+
+app.post('/softwarecount', (req, res) => {
+    conn.query("SELECT DISTINCT COUNT(Item.IDevice), Item.IDevice, Report.RHost FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE ((Item.IID)=1281) GROUP BY Item.IID, Item.IDevice, Report.RHost;").then(data => {
+        console.log(data)
+        res.render("softwarecount.html", { Report: data });
+    }).catch(err => console.log(err));
+})
+
+app.post('/softwarecount-computernames', (req, res) => {
+    console.log(req.body.submit)
+    conn.query("SELECT DISTINCT Item.IDevice, Report.RHost FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE Item.IDevice='" + req.body.submit + "'").then(data => {
+        console.log(data);
+        res.render("softwarecount-computername.html", { Report: data });
+    }).catch(err => console.log(err));
+})
 
 const listener = http.listen(3000, () => {
     console.log("Your app is listening on port " + 3000);
