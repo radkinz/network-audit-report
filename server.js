9385e3644af46ac915c7815bcabe04f9311908e8
Ruby M. Adkins <radkins22@students.stab.org>
2021-08-09 17:13:47 -0400

    handle search by department post req

--- server.js
+++ server.js
@@ -3,23 +3,20 @@ const app = express();
 const http = require("http").Server(app);
 const bodyParser = require("body-parser");
 
-//setup environment variables
-require('dotenv').config();
-const database_path = process.env.DATABASEPATH;
-
-//access the access database
-const ADODB = require('node-adodb');
-const conn = ADODB.open(database_path);
+app.use(express.static("public"));
 
 //setup app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-//access HTML files
 var engines = require("consolidate");
 app.engine("html", engines.hogan);
 app.set("views", __dirname + "/views");
+
+//access the access database
+const ADODB = require('node-adodb');
+const conn = ADODB.open(process.env.DATABASE_PATH);
 
 //home page 
 app.get('/', (req, res) => {
@@ -38,7 +35,6 @@ app.get('/pctrack', (req, res) => {
 app.post('/pctrackpost', (req, res) => {
     //insert into pc track database
     var sql = 'INSERT INTO PcTrack(UserName, ComputerName, SerialNumber, ESNNumber, Building, Department, Room, RemoteComputerName, Function, Instrument, Orientation, Browser, TimeSubmitted) VALUES ("' + req.body.username  + '","' + req.body.computername + '","' + req.body.serialnumber + '","' + req.body.ESNNumber + '","' + req.body.Building + '","' + req.body.Department + '","' + req.body.Room + '","' + req.body.RemoteComputerName + '","' + req.body.Function  + '","' + req.body.Instrument  + '","' + req.body.Orientation  + '","' + req.body.Browser + '","' + req.body.Timestamp + '")'
-
     conn
         .execute(sql, 'SELECT @@Identity AS id')
         .then(data => {
@@ -48,8 +44,6 @@ app.post('/pctrackpost', (req, res) => {
             console.error(error);
         });
 
-
-
     //go back to homepage
     res.redirect("/")
 })
@@ -57,6 +51,8 @@ app.post('/pctrackpost', (req, res) => {
 //display hardware
 app.post
     ('/hardware', (req, res) => {
+		console.log(req)
+        console.log("hello")
         //do not need to worry about sql injection because user cannot type in the input
         var sql = "SELECT DISTINCT Item.IField, Item.IValue, Report.RHost FROM Item INNER JOIN Report ON Report.ID = Item.ReportID WHERE Report.RHost='" + req.body.submit + "'AND ( (((Item.ReportID)=Report.ID) And ((Item.IID)=538)) Or (((Item.IID)=539)) Or (((Item.IID)=2581)) Or (((Item.IID)=518)) Or (((Item.IID)=520)) Or (((Item.IID)=528)) Or (((Item.IID)=530)) Or (((Item.IID)=1028)) Or (((Item.IID)=514)) Or (((Item.IID)=2583)) Or (((Item.IID)=534)) Or (((Item.IID)=517)) Or (((Item.IID)=3845)) Or (((Item.IID)=3847)) Or (((Item.IID)=540)) Or (((Item.IID)=3859)) Or (((Item.IID)=3864)) Or (((Item.IID)=2306)) Or (((Item.IID)=523)) Or (((Item.IID)=524)) Or (((Item.IID)=526)) Or (((Item.IID)=536)) Or (((Item.IID)=2582)));"
         conn.query(sql).then(data => {
@@ -85,6 +81,15 @@ app.post
         }).catch(err => console.log(err));
     })
 
+//get comp info from pc track by department
+app.post
+    ('/department', (req, res) => {
+        conn.query("SELECT DISTINCT PcTrack.ComputerName, PcTrack.SerialNumber, PcTrack.ESNNumber, PcTrack.Building, PcTrack.Department, PcTrack.Room, PcTrack.RemoteComputerName, PcTrack.Function, PcTrack.Instrument, PcTrack.Orientation, PcTrack.Browser FROM PcTrack WHERE PcTrack.Department='" + req.body.submit + "'").then(data => {
+            console.log(data);
+            res.render("pctrackdisplay.html", { Report: data });
+        }).catch(err => console.log(err));
+    })
+
 app.post('/printerinfo', (req, res) => {
     conn.query("SELECT DISTINCT Item.IField, Item.IValue, Item.IID, Report.RHost, Item.IDevice FROM Report INNER JOIN Item ON Report.ID = Item.ReportID WHERE (((Item.IID)=1028)) OR (((Item.IID)=536));").then(data => {
         console.log(data)
@@ -107,6 +112,5 @@ app.post('/softwarecount-computernames', (req, res) => {
     }).catch(err => console.log(err));
 })
 
-const listener = http.listen(3000, () => {
-    console.log("Your app is listening on port " + 3000);
-});
+//port from IIS server
+app.listen(process.env.PORT);
